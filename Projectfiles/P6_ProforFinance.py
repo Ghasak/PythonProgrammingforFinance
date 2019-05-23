c8e3a6c34fd8b3d72e4955b58365a59beaab1f7e
Ghasak Ibrahim <mdotgh@gmail.com>
2019-05-23 19:32:33 +0900

    Pulling all stocks from companies finished of 500

--- Projectfiles/P6_ProforFinance.py
+++ Projectfiles/P6_ProforFinance.py
@@ -63,18 +63,25 @@ def get_data_from_yahoo(reload_sp500 = False):
 
     start   = dt.datetime(2001,1,1)
     end     = dt.datetime(2016,12,31)
+    counter = 1
 
-    non_valid_company = ["BDX","BBT","CF","COST","DOW","EVRG","HIG"]
+    non_valid_company = ["BDX","BBT","CF","COST","DOW","EVRG","HIG", "COTY", "LIN", "OKE","JNJ","PNW","PPG","DGX","VMC","ZBH"]
     for ticker in tickers:
-        print(ticker)
+        print(counter,ticker)
+        #print("\n")
+        #os.system('clear')
         if ticker in non_valid_company:
             continue
         else:
             if not os.path.exists(ResourceDir+"/resources/stock_dfs/{}.csv".format(ticker)):
-                df = web.DataReader(ticker.replace('.','-'), 'yahoo', start, end)
+                counter = counter+1
+                df = web.DataReader(ticker, 'yahoo', start, end)
                 df.to_csv(ResourceDir+"/resources/stock_dfs/{}.csv".format(ticker))
             else:
                 print('Already have {}'.format(ticker))
+
+        if counter == len(tickers):
+            print("All companies are finished")
 
 
 get_data_from_yahoo()
