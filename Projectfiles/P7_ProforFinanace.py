e86a48f21b75d6fa5df5f1049d6cfad76752fb03
Ghasak Ibrahim <mdotgh@gmail.com>
2019-05-24 15:54:17 +0900

    Finished P7

---
+++ Projectfiles/P7_ProforFinanace.py
@@ -0,0 +1,122 @@
+# -----------------------------------------------------
+# Intro and Getting Stock Price Data
+# - Python Programming for Finance p.3
+# -----------------------------------------------------
+# -----------------------------------------------------
+import os
+import os.path as path
+# Resource file export
+# ResourceDir =  path.abspath(path.join(__file__ ,"../..")) # It doesn't work so far.
+# ResourceDir = path.abspath(path.join(os.getcwd(),"../..")) # To go two directories,
+# ResourceDir = path.abspath(path.join(os.getcwd(),"../"))    # To go one directory back.
+ResourceDir = os.getcwd()
+# https://stackoverflow.com/questions/27844088/python-get-directory-two-levels-up
+# -----------------------------------------------------
+# Installing packages -
+# -----------------------------------------------------
+# Exporting Direcotry : ResourceDir+ '/resources/
+import bs4 as bs
+import pickle
+import requests
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader as web
+
+
+# Les get the data from the wikipedia from the link of all companies and search for a specific table
+# You have to use the (Show inspection element of the html source file to know from where to start)
+
+def save_sp500_tickers():
+    resp    = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup    = bs.BeautifulSoup(resp.text,features="lxml") # Now you store the website .html file as a .txt file
+    table   = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]: # tr: table row for your current table
+        ticker = row.findAll('td')[0].text.replace('.','-') # getting the zero-column where the name of companies are.(column name: Ticker symbol), also as this is a soup object we convert it to a text.count
+        # mapping = str.maketrans(".","-")
+        # ticker = ticker.translate(mapping)
+        mapping = str.maketrans(".","-")
+        ticker = ticker.translate(mapping)
+
+        ticker = ticker.rstrip()
+        tickers.append(ticker)
+    # Now we will save the file we created as an pickle object to not request from the website everytime
+    with open(ResourceDir+"/resources/sp500tickers.pickle","wb") as f:
+        pickle.dump(tickers,f)
+
+    print(tickers)
+    return tickers
+
+#save_sp500_tickers()
+
+# -----------------------------------------------------
+def get_data_from_yahoo(reload_sp500 = False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open(ResourceDir+"/resources/sp500tickers.pickle","rb") as f: # rb for read the file
+            tickers = pickle.load(f)
+
+    if not os.path.exists(ResourceDir+"/resources/stock_dfs/"): # Create a directory to export data to it.
+        os.makedirs(ResourceDir+"/resources/stock_dfs/")
+
+    start   = dt.datetime(2001,1,1)
+    end     = dt.datetime(2016,12,31)
+    #counter = 1
+
+    non_valid_company = ["BDX","BBT","CF","COST","DOW","EVRG","EW","HIG", "COTY", "LIN", "OKE","JNJ","PNW","PPG","DGX","VMC","ZBH"]
+    tickers_len = len(tickers)-1
+    for index,ticker in enumerate(tickers):
+        print(index,ticker)
+        #print("\n")
+        #os.system('clear')
+        if ticker in non_valid_company:
+            continue
+        else:
+            if not os.path.exists(ResourceDir+"/resources/stock_dfs/{}.csv".format(ticker)):
+                #counter = counter+1
+                df = web.DataReader(ticker, 'yahoo', start, end)
+                df.to_csv(ResourceDir+"/resources/stock_dfs/{}.csv".format(ticker))
+            else:
+                print('Already have {}'.format(ticker))
+
+        if index == tickers_len:
+            print("All companies are finished")
+
+
+#get_data_from_yahoo()
+
+# -----------------------------------------------------
+non_valid_company = ["BDX","BBT","CF","COST","DOW","EVRG","EW","HIG", "COTY", "LIN", "OKE","JNJ","PNW","PPG","DGX","VMC","ZBH"]
+def compile_data():
+    with open(ResourceDir+"/resources/sp500tickers.pickle","rb") as f:
+        tickers = pickle.load(f)
+        print(tickers)
+        tickers = [item for item in tickers if item not in non_valid_company]
+        #tickers = tickers.remove(non_valid_company)
+
+    main_df = pd.DataFrame()
+
+    for count,ticker in enumerate(tickers):
+        df = pd.read_csv(ResourceDir+"/resources/stock_dfs/{}.csv".format(ticker))
+        df.set_index('Date', inplace = True)
+
+        df.rename(columns = {'Adj Close': ticker}, inplace = True) # changed the column name.
+        df.drop(['Open', 'High', 'Low', 'Close','Volume'], axis= 1, inplace = True)
+
+
+        if main_df.empty:
+            main_df = df
+
+        else:
+            main_df = main_df.join(df, how = 'outer')
+
+        if count % 10 == 0:
+            print(count)
+
+    print(main_df.head())
+    main_df.to_csv(ResourceDir+"/resources/sp500_joined_closes.csv")
+
+
+compile_data()
---
+++ Projectfiles/README_P7.md
@@ -0,0 +1,32 @@
+# Getting all company pricing data in the S&P 500
+## - Python Programming for Finance p.6
+
+Hello and welcome to part 7 of the Python for Finance tutorial series. In the previous tutorial, we grabbed the Yahoo Finance data for the entire S&P 500 of companies. In this tutorial, we're going to bring this data together into one DataFrame.
+
+# Output
+To Run this in terminal use
+
+```
+python Projectfiles/P7_ProforFinanace.py
+```
+![](./output_graphs/P7-1.png)
+Also you through VSCODE:
+![](./output_graphs/P7-2.png)
+# My Note
+We have got all the company information that we need, now we will combine them all together. Pulling all the (API) locally of all **Fortune SP 500** and it is better to pull all information together and store them. We will combine all the **Adjust Close**
+
+# Problem
+Similar to previous after we got all names of the companies the we were able to grab only the one online and store them locally. Therefore, the tickers should be updated to the only files we are having right now. To do so I approached this through removing the items ** company names** from the **tickers** list. There are several ways to do so such as **list.remove()** **list.pop()** and so on. read more here:
+https://www.quora.com/How-do-I-remove-an-item-from-a-python-list
+*Note: The only one was working is through **list-Comprehension** as following:
+
+```
+tickers = [item for item in tickers if item not in non_valid_company]
+```
+See here:
+https://stackoverflow.com/questions/4211209/remove-all-the-elements-that-occur-in-one-list-from-another
+
+
+# Inspiration
+
+https://pythonprogramming.net/sp500-company-price-data-python-programming-for-finance/
