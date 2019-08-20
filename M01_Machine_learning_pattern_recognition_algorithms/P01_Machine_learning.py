fb03c07b720f79a27a2ad92b69d8de576365f077
Ghasak Ibrahim <mdotgh@gmail.com>
2019-08-20 17:35:33 +0900

    finishing the platform

--- M01_Machine_learning_pattern_recognition_algorithms/P01_Machine_learning.py
+++ M01_Machine_learning_pattern_recognition_algorithms/P01_Machine_learning.py
@@ -24,8 +24,29 @@ def convert_date(date_bytes):
 
 
 def graphRaxFx():
+
+    ''' Getting the dataset from our current file
+        [Note] Possible output (The strpdate2num class was deprecated in Matplotlib 3.1 and will be removed in 3.3. Use time.strptime or dateutil.parser.parse or datestr2num instead.)
+    '''
     date,bid,ask = np.loadtxt(os.path.join(DATA_LOCATION,"GBPUSD1d.txt"),
                                                           unpack= True,
                                                           delimiter= ',',
                                                           converters={0:convert_date})
+    fig = plt.figure(figsize=(10,7))
+    ax1 = plt.subplot2grid((40,40), (0,0), rowspan= 40, colspan = 40)
+
+    ax1.plot(date,bid)
+    ax1.plot(date,ask)
+
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+
+    plt.grid(True)
+    plt.show()
+
+
+
+
+if __name__ == "__main__":
+    # Seeing the dataset
+    graphRaxFx()
 
