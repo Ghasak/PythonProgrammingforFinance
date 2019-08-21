5b0f186fa33a1ca5b3a66ea32045778c33e9a441
Ghasak Ibrahim <mdotgh@gmail.com>
2019-08-21 19:24:30 +0900

    Finished the session info class

--- M01_Machine_learning_pattern_recognition_algorithms/P06_Pattern_Finding_Storing.py
+++ M01_Machine_learning_pattern_recognition_algorithms/P06_Pattern_Finding_Storing.py
@@ -2,7 +2,7 @@
     Machine learning with finance - Pattern Recognition for
         Algorithmic Forex and Stock Trading: Finding Patterns: Pattern Finding and Storing:
 """
-message = "Pattern Finding and Storing: Machine Learning for Algorithmic Trading in Forex and Stocks Part 6"
+messagex = "Pattern Finding and Storing: Machine Learning for Algorithmic Trading in Forex and Stocks Part 6"
 
 import os
 import sys
@@ -117,16 +117,6 @@ def graphRaxFx():
 
 
 
-
-
-
-
-
-
-
-
-
-
 class session_info():
     ''' - This class is used just to organize our output'''
     # Class variables
@@ -141,11 +131,10 @@ class session_info():
         self.len_string_adding = len("Today's date:"+str(session_info.T0))
     # This method for showing starting running - time
     def Initial_run(self):
-
         self.len_string = len(self.your_string)
-        print(self.len_string*"=")
-        print(self.your_string)
-        print(self.len_string*"=")
+        # print(self.len_string*"=")
+        # print(self.your_string)
+        # print(self.len_string*"=")
         print(self.len_string_adding*"-")
         print("Today's date:", session_info.T0.strftime("%d/%m/%Y %H:%M:%S"))
         print(self.len_string_adding*"-")
@@ -154,15 +143,62 @@ class session_info():
     def Finishing_run(self):
         from datetime import datetime
         Tx = datetime.now() #date.today()
-
         TPassed = Tx - session_info.T0
-        print("\n")
-        print(self.len_string*"=")
-        print(self.your_string)
-        print(self.len_string*"=")
+        # print(self.len_string*"=")
+        # print(self.your_string)
+        # print(self.len_string*"=")
         print("Starting  time at = {}".format(self.T0.strftime("%d/%m/%Y %H:%M:%S")))
         print("Executing time at = {}".format(Tx.strftime("%d/%m/%Y %H:%M:%S")))
         print(f"[Done] exited with code = 0 in {TPassed} seconds")
+
+    def add_line(self):
+        """ This Function is used to type fancy printing
+            created for the purpose of isolating all the
+            commands and functions in python.
+        """
+        #N_spaces = message.count(' ') # From both sides
+        banner = 40
+        words = [self.your_string]
+        word_sum = sum(len(i) for i in words)
+        margin1 = int(banner/5)*"="
+        margi1_len = len(margin1)
+        margin2 = (banner-(margi1_len+word_sum+2))*"="
+        if len(margin1+self.your_string+margin2) < banner:
+            """
+            Here for simple one line sentence we add
+            directly the margins with the self.your_string
+            """
+            print(banner*"=")
+            print(f"""{margin1} {self.your_string} {margin2}""")
+            print(banner*"=")
+        else:
+            """
+            Here we check the number of words and reset the list and
+            spaces everytime we go to a new line until the self.your_string
+            is finished.
+            """
+            print(banner*"=")
+            wordsx = self.your_string.split()
+            xr = 4 # alignment added later (adjusted manually)
+            i = 0
+            spacex = 0
+            listwords = []
+            while i < len(wordsx):
+
+                listwords.append(wordsx[i])
+                if sum(len(i) for i in listwords) + spacex < banner-xr:
+                    print(wordsx[i], end = " ")
+                    spacex = spacex +1
+
+                else:
+                    print(wordsx[i])
+                    listwords = []
+                    spacex = 0
+
+                i = i +1
+
+            print("\n", end= "")
+            print(banner*"=")
 
 # Individual method to see the dataset column distributed without pandas
 def See_the_dataset():
@@ -171,54 +207,14 @@ def See_the_dataset():
         print(Table[0][index],"\t",Table[1][index],"\t",Table[2][index])
 
 
-
-
-messagex = "This is the sentence that I want to make it in two lines"
-
-def MyString(messagex):
-    lenString = len(messagex)
-    messageList = list(messagex)
-    messagewords = messagex.split(' ')
-    # print(lenString)
-    # print(messageList)
-    # print(messagewords)
-    # Define your boundary
-    boundary = 10
-    lineList = []
-    l = 0
-    i = 0
-    while i < len(messagewords):
-        lineList.append(messagewords[i])
-        if len(lineList)< boundary:
-            print(messagewords[i],end = " ")
-            i = i+1
-        else:
-            lineList = []
-            i = i +1
-
-
-
-
-
-
-MyString(messagex)
-
-
-
-
-
-
-
-
-
 # Starting point for our Python Script
 if __name__ == "__main__":
-    output1 = session_info(message)
+    output1 = session_info(messagex)
     output1.Initial_run()
+    output1.add_line()
     #graphRaxFx()
     #patternFinder()
     #patternStorage()
-
     output1.Finishing_run()
 
 
