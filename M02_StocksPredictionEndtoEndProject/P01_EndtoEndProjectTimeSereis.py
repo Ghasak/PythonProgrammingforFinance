54aad344fcbac65986168d9c02c23b80b01d2c4f
Ghasak Ibrahim <mdotgh@gmail.com>
2019-10-25 20:02:58 +0900

    applying the final model

--- M02_StocksPredictionEndtoEndProject/P01_EndtoEndProjectTimeSereis.py
+++ M02_StocksPredictionEndtoEndProject/P01_EndtoEndProjectTimeSereis.py
@@ -10,6 +10,8 @@
 import os
 import sys
 print(os.getcwd())
+# Running inside the directory of M02_Stocks...
+DATADIRECTORY = os.path.join(os.getcwd(), "DataSet/Superstore.xls")
 
 import warnings
 import itertools
@@ -26,4 +28,61 @@ matplotlib.rcParams['xtick.labelsize'] = 12
 matplotlib.rcParams['ytick.labelsize'] = 12
 matplotlib.rcParams['text.color']      = 'k'
 
+
+# starting from superstore sales data, we start from time series analysis and forecastign for the furniture sales
+
+df = pd.read_excel(DATADIRECTORY)
+furniture = df.loc[df['Category']== 'Furniture']
+furniture['Order Date'].min(), furniture['Order Date'].max()
+
+cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 'Customer ID', 'Customer Name', 'Segment', 'Country', 'City', 'State', 'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category', 'Product Name', 'Quantity', 'Discount', 'Profit']
+furniture.drop(cols, axis=1, inplace=True)
+furniture = furniture.sort_values('Order Date')
+furniture.isnull().sum()
+
+furniture = furniture.groupby('Order Date')['Sales'].sum().reset_index()
+furniture = furniture.set_index('Order Date')
+furniture.index
+
+y = furniture['Sales'].resample('MS').mean()
+
+y.plot(figsize = (15,6))
+plt.show()
+
+
+# We can visualize our data using a method called time-series decompositon that allows us to decompose our time series into three distinct components: trend, seasonality, and noise.
+
+from pylab import rcParams
+rcParams['figure.figsize'] = 18,8
+decomposition = sm.tsa.seasonal_decompose(y, model = 'additive')
+fig = decomposition.plot()
+plt.show()
+
+
+# Time series froecasting with ARIMA
+'''
+    We are going to apply one of the most commonly used method for time-series forecasting, known as ARIMA, which stands for Autoregressive Integrated Moving Average.
+'''
+
+p = d = q = range(0,2)
+pdq = list(itertools.product(p,d,q))
+seasonal_pdq = [(x[0],x[1],x[2],12) for x in list(itertools.product(p,d,q))]
+
+print('Examples of parameter combinations for Seasonal ARIMA ... ')
+print('SARIMAX: {} x {}'.format(pdq[1],seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1],seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2],seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2],seasonal_pdq[4]))
+
+
+for param in pdq:
+    for param_seasonal in seasonal_pdq:
+        try:
+            mod = sm.tsa.statespace.SARIMAX(y, order = param, seasonal_order = param_seasonal,enforce_stationarity = False, enforce_invertibility = False)
+
+            results = mod.fit()
+            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+        except:
+            continue
+        
 
